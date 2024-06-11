702639d1982a15788342f7d4c26f940eee754e9b
evercarlos <ecarlosr@alumno.unsm.edu.pe>
2024-06-10 23:08:42 -0500

    update controller

--- src/main/java/com/tecsofec/sysempresa/controller/v1/CompanyController.java
+++ src/main/java/com/tecsofec/sysempresa/controller/v1/CompanyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/company", produces = "application/json")
+@CrossOrigin("*")
 public class CompanyController {
 
     private final CompanyService companyService;
